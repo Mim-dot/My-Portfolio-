da9ab4c775abc746768889d2ee6cd79faa12d2aa
mim-dot <mimh84297@gmail.com>
2025-06-30 16:04:51 +0600

    logo added

--- src/Pages/Nav.jsx
+++ src/Pages/Nav.jsx
@@ -3,10 +3,18 @@ import React from "react";
 const Nav = () => {
   return (
     <nav className="sticky top-0 z-50 bg-white/30 backdrop-blur-lg shadow-md py-4 px-6 flex items-center justify-between w- rounded-3xl">
-      {/* Logo */}
-      <div className="text-2xl font-extrabold text-gray-800 tracking-wide">
-        Cloud<span className="text-orange-500">Ramp</span>
-      </div>
+    <div className="flex items-center gap-3">
+  {/* Logo Icon */}
+  <div className="w-14 h-14 bg-gradient-to-br from-blue-600 to-indigo-700 text-white flex items-center justify-center rounded-full font-extrabold text-xl shadow-lg tracking-wider uppercase">
+    MH
+  </div>
+
+  {/* Brand Name */}
+  <div className="text-2xl font-extrabold text-gray-800 tracking-wide">
+    Cloud<span className="text-orange-500">Ramp</span>
+  </div>
+</div>
+
 
       {/* Navigation Links */}
       <div className="space-x-6 hidden md:flex">
